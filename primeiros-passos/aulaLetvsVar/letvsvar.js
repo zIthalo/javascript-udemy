53fb65724d1bcf2ecb3ff95483e9fa396ea24957
zIthalo <ithalowillian@gmail.com>
2022-10-14 18:06:45 -0300

    Update letvsvar.js

---
+++ primeiros-passos/aulaLetvsVar/letvsvar.js
@@ -0,0 +1,13 @@
+var nome = 'ithalo' //a diferença principal é que o var eu posso mudar o conteúdo várias vezes já o let não
+var nome = 'willian'
+console.log(nome)
+// com let eu não posso fazer o que eu fiz dá um erro logo de cara
+/**
+ * let name = 'ithalo'
+   let name = 'willian'
+
+ */
+/**
+ * apartir de hoje só use let >:(!!!
+ */
+
