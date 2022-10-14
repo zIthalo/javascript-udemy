c6eef72bab7fa2903b21a3dab2b270f918506701
zIthalo <ithalowillian@gmail.com>
2022-10-14 18:01:26 -0300

    Novos exercícios

--- primeiros-passos/script.js
+++ primeiros-passos/script.js
@@ -14,3 +14,16 @@ console.log(`Olá! Meu nome é "Ithalo". Estou aprendendo Java Script às`, 15,'
  * 
  * 
  */
+
+/**IMC em js */
+
+const nome = 'ithalo' //tudo que for string deve estar entre aspas
+const sobrenome = 'willian'
+const idade = 25
+const peso = 84
+const alturaEmM = 1.79
+let indiceMassaCorporal = peso / (alturaEmM * alturaEmM)
+let anoNasc = 2022 - idade
+
+console.log(`${nome} ${sobrenome} tem ${idade} anos, pesa ${peso}kg`)
+console.log(`tem ${alturaEmM} de altura e seu IMC é de ${indiceMassaCorporal}`)
