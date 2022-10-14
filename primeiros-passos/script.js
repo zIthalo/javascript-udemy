0199e394b9d45c9d34acdd6e186de91ab7f04a93
zIthalo <ithalowillian@gmail.com>
2022-10-14 16:10:15 -0300

    Create script.js

---
+++ primeiros-passos/script.js
@@ -0,0 +1,10 @@
+console.log('Olá, mundo!') //isto é uma função comando escreval em java script
+
+//declarando variáveis
+// você quando utilizar aspas simples você pode até colocar aspas duplas em alguma palavra dentro das strings
+
+//com aspas duplas você pode colocar aspas simples
+
+//com crase você pode usar tanto as simples quanto as duplas e, inclusive utilizar template strings ${}
+
+console.log(`Olá! Meu nome é "Ithalo". Estou aprendendo Java Script às`, 15,'da tarde')
