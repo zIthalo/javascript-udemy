e7914874f7b729c5bde18588254df4e88eb4e9fd
zIthalo <ithalowillian@gmail.com>
2022-10-14 18:14:06 -0300

    Update tipos.js

---
+++ primeiros-passos/Tipos-primitivos/tipos.js
@@ -0,0 +1,8 @@
+const nome = 'ithalo' 
+const nome1 = "ithalo" //Strings ↑↓
+const nome2 = `Ithalo`
+const n1 = 10 //Number
+const n2 = 18.5 //Number
+let nomeAluno //undefined → não aponta para nenhum lugar na memória
+let sobrenomeAluno = null //→ não aponta para nenhum lugar na memória
+
