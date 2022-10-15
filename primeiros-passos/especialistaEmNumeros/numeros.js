1961e1e2af487373ccfc7398c7f9ff6c2db17d24
zIthalo <ithalowillian@gmail.com>
2022-10-15 15:37:22 -0300

    Atualizando

---
+++ primeiros-passos/especialistaEmNumeros/numeros.js
@@ -0,0 +1,11 @@
+let n1 = 15
+let n2 = 2.5
+
+/*passando para string para concatenar
+console.log(n1.toString() + n2)
+saída: 152.5
+transformando o n1 em string definitivamente
+n1 = n1.toString();
+
+
+*/
