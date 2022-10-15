d61f8dea47b5f0a48ac30c4386a7c781e097a350
zIthalo <ithalowillian@gmail.com>
2022-10-15 10:11:43 -0300

    Update strings.js

--- primeiros-passos/especialistaEmStrings/strings.js
+++ primeiros-passos/especialistaEmStrings/strings.js
@@ -25,25 +25,30 @@ console.log(umastring.concat(' ', 'em', ' ', 'um lindo dia'))
 a saída seria:
 Um texto em um lindo dia
 
-mas faça isso com template strings que ficará muito mais fácil assim:
+**mas faça isso com template strings que ficará muito mais fácil, assim:
 
 console.log(`${umastring} em um lindo dia`) ;)
 
-Se você quiser saber em qual índice começa uma palavra faça:
+--Se você quiser saber em qual índice começa uma palavra faça: (também serve para achar letras)
 
 console.log(umastring.indexOf('texto'))
 
 vai retornar 3
 
-Se você quisesse saber qual o índice de trás para frente:
+console.log(umastring.indexOf('o', 3))
+
+Lê-se: Quero achar a primeira letra 'o' apartir do índice 3
+
+--Se você quisesse saber qual o índice de trás para frente:
 
 console.log(umastring.lastIndexOf('t', 5))
-lê-se quero saber onde encontro uma letra t do índice 5 até o 0
 
-Se você quiser substituir uma palávra de um texto faça assim:
+lê-se: quero saber onde encontro uma letra t do índice 5 até o 0
+
+Se você quiser substituir uma palavra de um texto faça assim:
 console.log(umastring.replace('um', 'outro'))
 
-Lê-se: subistitua um por outro
+Lê-se: subistitua 'um' por 'outro'
 
 Se você quiser pegar uma palavra específica da sua string faça:
 
@@ -51,13 +56,20 @@ console.log(umastring.slice(1,5))
 
 Lê-se: Pegue tudo que estiver do índice 1 ao índice 6 
 
-Tudo em MAIÙSCULAS:
+Tudo em MAIÚSCULAS:
 
 console.log(umastring.tuUpperCase())
 
 Tudo em Minúsculas
 
 console.log(umastring.toLowerCase())
+
+se você quiser ver quantas palavras tem na sua digitação
+console.log(umastring.split(' '))
+
+Entende-se que este comando vai retornar uma array somente com as palavras digitadas mas sem os espaços entre elas
+
+Se você quiser mostrar as palavras sem alguma letra ou palavra é só digitar entre as aspas qual palvra ou letra você quer retirar
 
 para ver todos esses comandos você pode ir no site w3schools, no site da mozilla e pesquisar pela documentação js
 
