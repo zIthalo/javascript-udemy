b413c886ac04f9f866d64d45ff5623050e7f494a
zIthalo <ithalowillian@gmail.com>
2022-10-15 19:34:24 -0300

    Novos Exercícios

---
+++ primeiros-passos/ObjetoMath/math.js
@@ -0,0 +1,51 @@
+//Comando para arredondar um número para baixo função.método ↓
+// let n1 = 9.54281
+// let n2 = Math.floor(n1)
+// console.log(n2)
+
+//Para arredondar para cima faça
+// let n1 = 9.54281
+// let n2 = Math.ceil(n1)
+// console.log(n2)
+
+//Como arredondar para o mais proximo? ↓
+// let n1 = 9.54281
+// let n2 = Math.round(n1)
+// console.log(n2)
+
+// //como achar o maior número?
+// console.log(Math.max(1,2,3,4,5,-10,-50,1500,9,8,74,6))
+
+// //como achar o menor número?
+// console.log(Math.min(1,2,3,4,5,-10,-50,1500,9,8,74,6))
+
+//como gerar um número aleatório?
+// console.log(Math.random())
+
+//Como gerar numeros aleatórios entre um número e outro número já arredondado para o mais próximo
+// const aleatorio = Math.round(Math.random() * (10 - 5) + 5)
+//Lê-se: pegue um número aleatório (Math.random) entre 10 e 5
+// console.log(aleatorio)
+
+//Fazendo potênciação com math
+//console.log(Math.pow(2,10))
+
+
+//lê-se: Faça a potência (Math.pow) de dois elevado a 10 (2,10)
+
+//Mas faça assim que é melhor console.log(2 ** 10)
+
+//Tirando raiz quadrada de 2 formas
+
+// let n1 = 9
+// console.log(n1 ** (1/2)) //Lê-se: n1 elevado a um dividido por 2
+
+//Ou
+// let n1 = 9
+// console.log(n1 ** 0.5) //vai dar o mesmo resultado
+
+//Algo que dá erro em outras linguagens mas em Js não
+
+console.log(100/0) //aparece o valor infinity porque o número fica gicantesco se for conferir ele dá verdadeiro
+
+
--- primeiros-passos/especialistaEmNumeros/numeros.js
+++ primeiros-passos/especialistaEmNumeros/numeros.js
@@ -1,5 +1,5 @@
-let n1 = 15
-let n2 = 2.5
+let n1 = 0.7
+let n2 = 0.1
 
 /*passando para string para concatenar
 console.log(n1.toString() + n2)
@@ -7,5 +7,73 @@ saída: 152.5
 transformando o n1 em string definitivamente
 n1 = n1.toString();
 
+//Passando meu n1 para binário
+console.log(n1.toString(2))
+
+//Mostrar apenas duas casas decimais depois do ponto
+console.log(n1.toFixed(2))
+
+//Perguntar ao js se um número recebido de uma base de dados é inteiro retornará true ou false
+console.log(Number.isInteger(n2))
+
+//Perguntando ao js se determinado valor não é um número retornará true.
+
+let temp = n1 * 'Oi'
+console.log(Number.isNaN(temp))
+
+//IEEE 74-2008 Um bagui estranho do JS
+//era para ter
+n1 += n2 //0.8
+n1 += n2 //0.9
+n1 += n2 //1.0
+console.log(n1)
+//O resultado será 0.9999999999999999
+
+
+//IEEE 74-2008 Um bagui estranho do JS
+//era para ter
+n1 += n2 //0.8
+n1 += n2 //0.9
+n1 += n2 //1.0
+
+n1 = n1.toFixed(2) //fazer assim não irá fazer o número virar inteiro
+console.log(n1)
+console.log(Number.isInteger(1.00)) //1.0 é realmente um número inteiro
+//mas o n1 ainda não é inteiro
+
+
+//E como resolver este mesmo problema com contas?
+
+n1 = ((n1 * 100) + (n2 * 100)) / 100 //0.8
+n1 = ((n1 * 100) + (n2 * 100)) / 100 //0.9
+n1 = ((n1 * 100) + (n2 * 100)) / 100 //1.0
+
+n1 = Number(n1.toFixed(2)) //Assim o n1 ficará inteiro
+console.log(n1)
+console.log(Number.isInteger(n1)) //agora sim ele será considerado inteiro
+
+
+//resolvendo com expressões facilita o entendimento do código
+
+n1 += n2 //0.8
+n1 += n2 //0.9
+n1 += n2 //1.0
+
+n1 = Number(n1.toFixed(2)) //Assim o n1 ficará inteiro
+console.log(n1)
+console.log(Number.isInteger(n1)) //agora sim ele será considerado inteiro
 
 */
+
+//resolvendo com expressões facilita o entendimento do código
+
+n1 += n2 //0.8
+n1 += n2 //0.9
+n1 += n2 //1.0
+
+n1 = Number(n1.toFixed(2)) //Assim o n1 ficará inteiro
+console.log(n1)
+console.log(Number.isInteger(n1)) //agora sim ele será considerado inteiro
+
+
+
