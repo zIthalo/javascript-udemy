4fb497f66c14e1ab4d6ce748f4a8bd675b9d1fe7
zIthalo <ithalowillian@gmail.com>
2022-10-22 09:49:08 -0300

    Atualizando

--- primeiros-passos/exercicio-numbers-math/numMath.js
+++ primeiros-passos/exercicio-numbers-math/numMath.js
@@ -1,26 +1,18 @@
-const n1 = Number(prompt('Digite um número: '))
+const n1 = Number(prompt('Digite um número: ')) //sempre transforme um pompt para number se você quser colocar valores numéricos nele, pois ele sempre retornará valores em string
 const numeroTitulo = document.getElementById('numero-titulo')
 const texto = document.getElementById('texto')
-// const raizQ
-// const inteiro
-// const nan
-// const arredondadoBaixo
-// const arredondadoAlto
-// const duasCasasDec
 
-//document.body.style = ('font-family:arial;')
+document.body.style = ('font-family:arial;')
 
-numeroTitulo.innerHTML = n1
+numeroTitulo.innerHTML = n1;
+texto.innerHTML = '';
+texto.innerHTML +=` <p>A raíz quadrada de ${n1} é: ${n1 ** 0.5}</p> <br>`;
 
-texto.innerHTML = ' '
+texto.innerHTML += `<p>${n1} é inteiro: ${Math.isInteger(n1)}</p> <br>`;
 
-texto.innerHTML +=` <p>A raíz quadrada de ${n1} é: ${n1 ** 0.5}</p> `
+texto.innerHTML += `<p>É NaN: ${Number.isNaN(n1)}</p> <br>`;
 
-texto.innerHTML += `<p>${n1} é inteiro: ${Math.isInteger(n1)}</p> `
+texto.innerHTML +=`<p>Este número arredondado para baixo fica: ${Math.floor(n1)}</p> <br>`;
 
-texto.innerHTML += `<p>É NaN: ${Number.isNaN(n1)}</p>`
-
-texto.innerHTML +=`<p>Este número arredondado para baixo fica: ${Math.floor(n1)}</p>`
-
-texto.innerHTML += `<p>Este número arredondado para cima fica ${Math.ceil(n1)}</p>`
-texto.innerHTML += `<p>Com duas casas decimais fica: ${n1.toFixed(2)}</p>`
+texto.innerHTML += `<p>Este número arredondado para cima fica ${Math.ceil(n1)}</p> <br>`;
+texto.innerHTML += `<p>Com duas casas decimais fica: ${n1.toFixed(2)}</p> <br>`;
